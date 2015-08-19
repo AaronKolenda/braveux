be674a7b257774482d29203bbb8d45a37b4c9774
AaronKolenda <kolenda.a@gmail.com>
2015-08-19 16:51:22 -0400

    adding password validation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+
   var login = document.getElementById("login");
 
 	login.onclick = function(){
@@ -9,15 +10,44 @@ document.addEventListener("DOMContentLoaded", function() {
 			var passwordWrap = document.getElementById("password-wrap");
 			var usernameWrap = document.getElementById("username-wrap");
 
-			if (username.length <= 0) {
-				warning.style.display = 'block';
-				console.log(warning);
-				passwordWrap.style.border = "2px solid rgb(236, 28, 35)";
-				usernameWrap.style.border = "2px solid rgb(236, 28, 35)";
+			var nonAlphaNumeric = checkAlphaNumeric(password);
+			var capital = hasUpperCase(password);
+			var sixChars = false;
+
+			if (password.length >= 6) {
+				sixChars = true;
 			}
 
-	        return false;
+			if (nonAlphaNumeric === true && capital === true && sixChars === true && username.length > 0) {
+				warning.style.display = 'none';
+				passwordWrap.style.border = "none";
+				usernameWrap.style.border = "none";
+				return true;
+			}
+			else {
+				displayWarning(warning, passwordWrap, usernameWrap);
+				return false;
+			}
+
 	    };
+
 });
+
+function checkAlphaNumeric(string){
+    if( /[^a-zA-Z0-9]/.test(string) ) {
+       return true;
+    }
+    return false;     
+}
+
+function hasUpperCase(string) {
+    return (/[A-Z]/.test(string));
+}
+
+function displayWarning(el, inputel1, inputel2) {
+	el.style.display = 'block';
+	inputel1.style.border = "2px solid rgb(236, 28, 35)";
+	inputel2.style.border = "2px solid rgb(236, 28, 35)";
+}
 
 
