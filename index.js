bef7923258bd870f7735abecd9d36203b3e3547f
AaronKolenda <kolenda.a@gmail.com>
2015-08-19 18:24:26 -0400

    adding password toggling

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var login = document.getElementById("login");
+  	//The following function validates the username and password input
 
+  	var login = document.getElementById("login");
 	login.onclick = function(){
 
 	        var username = document.getElementById("username").value;
@@ -31,6 +32,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	    };
 
+	    //The following toggles the menu to appear and disappear
+
 		var menuImg = document.getElementsByClassName("menu-img");
 		var menuOne = menuImg[0];
 		var menuTwo = menuImg[1];
@@ -44,7 +47,16 @@ document.addEventListener("DOMContentLoaded", function() {
 	    menuTwo.onclick = function(){
 	    	menu.style.display = 'none';
 	    }
+
+	    //The following toggles the password input type from password to text when the eye is clicked
+
+	    var eye = document.getElementById("eye");
+	    eye.onclick = function(){
+	    	togglePasswordFieldClicked();
+	    }
 });
+
+//This function checks if a string has an alphanumeric character, and returns true if it does
 
 function checkAlphaNumeric(string){
     if( /[^a-zA-Z0-9]/.test(string) ) {
@@ -53,14 +65,36 @@ function checkAlphaNumeric(string){
     return false;     
 }
 
+//This function checks if a string has an uppercase character, and returns true if it does
+
 function hasUpperCase(string) {
     return (/[A-Z]/.test(string));
 }
+
+//This function displays the warning error div, 
+//notifying the user that their username or password isn't valid
 
 function displayWarning(el, inputel1, inputel2) {
 	el.style.display = 'block';
 	inputel1.style.border = "2px solid rgb(236, 28, 35)";
 	inputel2.style.border = "2px solid rgb(236, 28, 35)";
 }
+
+//This function toggles the password input type between password and text
+
+function togglePasswordFieldClicked() {
+    var passwordField = document.getElementById('password');
+    var value = passwordField.value;
+
+    if(passwordField.type == 'password') {
+        passwordField.type = 'text';
+    }
+
+    else {
+        passwordField.type = 'password';
+    }
+    passwordField.value = value;
+}
+
 
 
